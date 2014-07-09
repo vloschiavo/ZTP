c4ab36feaab40011621dc88034a1e27ff3f975d6
Vince Loschiavo <vloschiavo@gmail.com>
2014-07-09 13:55:16 -0700

    working session 1pm

--- new_device.py
+++ new_device.py
@@ -3,13 +3,29 @@ import sys
 from glob import glob
 from jinja2 import Template
 
+# Number of yaml files
+y = 2
+
 # YAML file.
-with open(glob('*.yml')[0]) as fh:
-    data = yaml.load(fh.read())
+for x in range(1,y+1):
+	yamlfilename = 'ex4200-0' + `x` + ".yml";
+	conffilename = 'ex4200-0' + `x` + ".conf";
 
-# Jinja2 template file.
-with open(glob('*.j2')[0]) as t_fh:
-    t_format = t_fh.read()
+	print x
+	print conffilename
 
-template = Template(t_format)
-print (template.render(data))
+	with open(yamlfilename) as fh:
+	    data = yaml.load(fh.read())
+
+	# Jinja2 template file.
+	with open(glob('*.j2')[0]) as t_fh:
+	    t_format = t_fh.read()
+
+	template = Template(t_format)
+
+	fout = open(conffilename, 'w')
+	print fout
+	fout.write((template.render(data)))
+	fout.close()
+
+print "Good bye!"
